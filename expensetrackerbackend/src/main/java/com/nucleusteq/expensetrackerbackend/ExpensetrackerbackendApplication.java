2502ea258527e12c78c60e6af61480e6cdbbfe09
Nayan Awasthi <nayanawasthi973@gmail.com>
2025-03-22 09:53:23 +0530

    database configured

---
+++ expensetrackerbackend/src/main/java/com/nucleusteq/expensetrackerbackend/ExpensetrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nucleusteq.expensetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensetrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerbackendApplication.class, args);
+	}
+
+}
---
+++ expensetrackerbackend/src/test/java/com/nucleusteq/expensetrackerbackend/ExpensetrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleusteq.expensetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
